0e1b12004b7a3869c5b7f7b7ad8a5ab63bc61c24
Felipe Balbi <febalbi@microsoft.com>
2024-10-10 09:51:13 -0700

    Initial commit
    Signed-off-by: Felipe Balbi <febalbi@microsoft.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Embedded Power Sequence
+
+This crate provides and abstraction over SoC Power Sequencing to
+simplify the task of powering an SoC on and off. It's mainly targeted
+at the implementation of Embedded Controller Firmwares.
---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+//! This crate provides a Hardware Abstraction Layer for SoC Power Sequencing.
+
+#![doc(html_root_url = "https://docs.rs/embedded-keyboard/latest")]
+#![cfg_attr(not(test), no_std)]
+#![allow(async_fn_in_trait)]
+
+/// Power Sequence error.
+pub trait Error: core::fmt::Debug {
+    /// Convert error to a generic Power Sequence error kind.
+    ///
+    /// By using this method, Power Sequence errors freely defined by HAL
+    /// implementations can be converted to a set of generic Power Sequence
+    /// errors upon which generic code can act.
+    fn kind(&self) -> ErrorKind;
+}
+
+/// Power Sequence error kind.
+///
+/// This represents a common set of errors. HAL implementations are free to
+/// define more specific or additional error types. However, by providing a
+/// mapping to these common Power Sequence errors, generic code can still react
+/// to them.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[non_exhaustive]
+pub enum ErrorKind {
+    /// A different error occurred. The original error may contain more
+    /// information.
+    Other,
+}
+
+impl Error for ErrorKind {
+    #[inline]
+    fn kind(&self) -> ErrorKind {
+        *self
+    }
+}
+
+impl core::fmt::Display for ErrorKind {
+    #[inline]
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::Other => write!(
+                f,
+                "A different error occurred. The original error may contain more information"
+            ),
+        }
+    }
+}
+
+/// Power Sequence error type trait.
+///
+/// This just defines the error type, to be used by the other traits.
+pub trait ErrorType {
+    /// Error type
+    type Error: Error;
+}
+
+impl<T: ErrorType + ?Sized> ErrorType for &mut T {
+    type Error = T::Error;
+}
+
+impl Error for core::convert::Infallible {
+    #[inline]
+    fn kind(&self) -> ErrorKind {
+        match *self {}
+    }
+}
+
+pub trait PowerSequence: ErrorType {
+    async fn power_on(&mut self) -> Result<(), Self::Error>;
+    async fn power_off(&mut self) -> Result<(), Self::Error>;
+}
+
+impl<T: PowerSequence + ?Sized> PowerSequence for &mut T {
+    #[inline]
+    async fn power_on(&mut self) -> Result<(), Self::Error> {
+        T::power_on(self).await
+    }
+
+    #[inline]
+    async fn power_off(&mut self) -> Result<(), Self::Error> {
+        T::power_off(self).await
+    }
+}
